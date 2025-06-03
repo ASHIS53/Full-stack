3cf222fbd1c0afdbefa0924738c1184fe1c2a605
Aashish Verma <aashishverma1272@gmail.com>
2025-06-03 14:44:35 +0530

    add register user

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors"; 
 
 const app = express();
 
@@ -13,6 +14,12 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users",userRouter)
 
 export { app };
  
